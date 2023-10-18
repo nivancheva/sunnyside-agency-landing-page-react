d0036244c1e57b8fb04d3f4044d5b1ee11ba0971
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-10-18 11:24:39 +0300

    basic html to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,24 @@
+
+const links= [
+    {text:'About', url:'#'},
+    {text:'Services', url:'#'},
+    {text:'Projects', url:'#'}
+]
+
+
 export default function Header() {
     return (
         <div>
-            This is Header
+
+            <ul>
+                {links.map((link, idx) => {
+                    return (
+                        <li key={idx}><a className='links' href={link.url}>{link.text}</a></li>
+                    )
+                })}
+            </ul>
+
+            <button className="button">Contact</button>
         </div>
     )
 }
