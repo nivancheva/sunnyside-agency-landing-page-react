8638eac54d8b0440e82ea104ca54906727dd433e
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-10-18 15:06:31 +0300

    basic style to navigation bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,7 @@
+import './Header.css';
+import logo from '../images/logo.svg';
+import menuHamburder from '../images/icon-hamburger.svg';
+import { useState } from 'react';
 
 const links= [
     {text:'About', url:'#'},
@@ -7,18 +11,37 @@ const links= [
 
 
 export default function Header() {
+    const [menuVisible, setMenuVisible] = useState(false);
+
+    function toggleMenu() {
+        setMenuVisible(!menuVisible);
+    }
+
     return (
-        <div>
+        <div className='header-wrapper'>
+            <header className='header'>
+                <picture><img src={logo} alt='logo'/></picture>
+                <div className='navigation-bar flex-container'>
+                    <nav>
+                        <ul className='links-nav flex-container'>
+                            {links.map((link, idx) => {
+                                return (
+                                    <li key={idx}><a className='links' href={link.url}>{link.text}</a></li>
+                                )
+                            })}
+                        </ul>
+                    </nav>
+                    <button className="button">Contact</button>
+                </div>
+            
+                <button onClick={toggleMenu} className={menuVisible ? 'button-toggle-cliked' : 'button-toggle'}>
+                    <img src={menuHamburder}/>
+                </button>           
+            </header>
 
-            <ul>
-                {links.map((link, idx) => {
-                    return (
-                        <li key={idx}><a className='links' href={link.url}>{link.text}</a></li>
-                    )
-                })}
-            </ul>
-
-            <button className="button">Contact</button>
+            <div>
+                <h1>We are creatives</h1>
+            </div>
         </div>
     )
 }
