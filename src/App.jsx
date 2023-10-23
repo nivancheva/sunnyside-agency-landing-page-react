651fffbcf4171fc3a5a5f1a8e6006970f3b04bc1
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-10-23 11:42:44 +0300

    add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
-import Header from './components/Header'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
 
   return (
     <div>
       <Header />
+
+      <Footer />
     </div>
   )
 }
